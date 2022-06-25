ae654c286c47482ceca943a950e2ad33e72a32b0
gr-rishad <gr.rishad@gmail.com>
2022-06-25 17:32:15 +0600

    exception handling

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.springboot.blog.springbootblogrestapi.exception;
+
+import com.springboot.blog.springbootblogrestapi.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // handle specific exception
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    // global Exception
+}
